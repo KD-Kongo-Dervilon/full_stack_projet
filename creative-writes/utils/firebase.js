4a0c74aefb04f891e3380728bb11fe3feaf59510
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2022-11-07 18:09:06 +0100

    instal firebase-hooks
    setting up authentication

--- creative-writes/utils/firebase.js
+++ creative-writes/utils/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase';
-import { getFireStore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,11 +15,9 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_APP_MESUREAMENT
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth();
-export const db = getFireStore(app);
+export const db = getFirestore(app);
