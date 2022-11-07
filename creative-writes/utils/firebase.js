26fe0da45d667ca34b2301abf8fa46589a7d4c11
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2022-11-07 16:45:18 +0100

    end of browsing

--- creative-writes/utils/firebase.js
+++ creative-writes/utils/firebase.js
@@ -10,13 +10,13 @@ import { getFireStore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDXO--tVqEyKcfI1Q0RJZS6yQbKeZP0PCM",
-    authDomain: "fullstack-react-8c32d.firebaseapp.com",
-    projectId: "fullstack-react-8c32d",
-    storageBucket: "fullstack-react-8c32d.appspot.com",
-    messagingSenderId: "881929837517",
-    appId: "1:881929837517:web:b979b7b304ad46e04ac284",
-    measurementId: "G-2M9KDM5STV"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_APP_MESUREAMENT
 };
 
 // Initialize Firebase
